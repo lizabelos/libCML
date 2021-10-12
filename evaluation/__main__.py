5a797bf236749085ad72885da0ad32002d56a427
thomas <belos.thomas@oulook.com>
2021-10-12 14:58:43 +0200

    use the table class to export the data of multiple slams

--- evaluation/__main__.py
+++ evaluation/__main__.py
@@ -6,6 +6,7 @@ from evo.tools.file_interface import csv_read_matrix
 import dataset
 import evaluator
 import slam
+from table import FileTable
 
 
 def main():
@@ -18,12 +19,14 @@ def main():
 
     datasets = []
     slams = []
+    slams_names = []
 
     for line in content:
         if len(line) == 0:
             continue
         if line[0] == "ModSLAM":
             slams.append([slam.ModSLAM, line[1]])
+            slams_names.append(line[0])
         elif line[0] == "TUM":
             datasets = datasets + dataset.TUM(line[1])
         elif line[0] == "KITTI":
@@ -31,10 +34,15 @@ def main():
         else:
             print("Unknown dataset type : " + line[0])
 
+    datasets_names = [x.name() for x in datasets]
+    table = FileTable(["ate of " + x for x in slams_names] + ["rpe of " + x for x in slams_names], datasets_names, "result.csv")
+
     print("Found " + str(len(datasets)) + " videos")
 
     for i in range(0, len(datasets)):
-        for s in slams:
+        for j in range(0, len(slams)):
+            s = slams[j]
+            name = slams_names[j]
             context = s[0](s[1])
 
             print("Evaluating on " + datasets[i].name())
@@ -42,12 +50,17 @@ def main():
 
             context.run(datasets[i])
 
-            #try:
-            evaluation = evaluator.fromslam(context)
-            print("ATE of " + datasets[i].name() + ": " + str(evaluation.ape_rmse()))
-            print("RPE of " + datasets[i].name() + ": " + str(evaluation.rpe_rmse()))
-            #except:
-            #print("Unable to evaluate " + datasets[i].name())
+            try:
+                evaluation = evaluator.fromslam(context)
+                ate = evaluation.ape_rmse()
+                rpe = evaluation.rpe_rmse()
+                print("ATE of " + datasets[i].name() + ": " + str(ate))
+                print("RPE of " + datasets[i].name() + ": " + str(rpe))
+
+                table.set("ate of " + name, datasets[i].name(), ate)
+                table.set("rpe of " + name, datasets[i].name(), ate)
+            except:
+                print("Unable to evaluate " + datasets[i].name())
 
 
 if __name__ == "__main__":
